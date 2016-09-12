9d8fcccd4e687ff725231512d9aaea148833b8d0
kdc6 <kdc6@rice.edu>
2016-09-11 22:15:13 -0500

    Add files via upload

---
+++ python-plus.py
@@ -0,0 +1,6 @@
+import re
+
+string1="Hello, world."
+if re.search(r"l+", string1):
+	print 'There are one or more consecutive letter "l"' +\
+		"'s in "  + string1
---
+++ python-question.py
@@ -0,0 +1,6 @@
+import re
+
+string1 = "Hello, world."
+if re.search(r"H.?e", string1):
+	print "There is an 'H' an a 'e' separated by " +\
+		"0-1 characters (Ex: He Hoe) \n" 
